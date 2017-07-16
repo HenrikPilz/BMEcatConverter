a7474ca540dc469106ae0faa32c7704124ee0569
Henrik Pilz <henrik.pilz@gmail.com>
2017-07-16 15:47:06 +0200

    Added validation with raising Exceptions

--- src/data/feature.py
+++ src/data/feature.py
@@ -3,9 +3,9 @@ Created on 05.05.2017
 
 @author: henrik.pilz
 '''
-import logging
+from src.data import ValidatingObject
 
-class Feature():
+class Feature(ValidatingObject):
     def __init__(self):
         self.name = None
         self.values = []
@@ -14,17 +14,25 @@ class Feature():
         self.description = None
         self.valueDetails = None
 
-    def validate(self):
-        if self.name is None:
-            logging.error("Der Merkmalsname fehlt.")
-        if  self.values is not None and len(self.values) > 0 and self.variants is not None:
-            logging.error("Es wurden Values und Varianten angegeben. Die Zuordnung ist mehrdeutig.")
+    def validate(self, raiseException=False):
+        errMsg = None
+        if self.name is None or len(self.name.strip()) == 0:
+            errMsg = "Der Merkmalsname fehlt."
+            super().logError(errMsg, raiseException)
+                
+        if (self.values is None or len(self.values) == 0) and (self.variants is None or len(self.variants) == 0):
+            errMsg = "Es wurden weder Attributswerte noch Varianten angegeben."
+            super().logError(errMsg, raiseException)
+        elif len(self.values) > 0 and self.variants is not None and len(self.variants) > 0:
+            errMsg = "Es wurden Values und Varianten angegeben. Die Zuordnung ist mehrdeutig."
+            super().logError(errMsg, raiseException)
         else:
             if self.variants is not None:
                 self.variants.validate()
     
     def addValue(self, value):
-        self.values.append(value)
+        if value is not None and len(value.strip()) > 0:
+            self.values.append(value)
 
     def addVariant(self, variant):
         self.variants.addVariant(variant)
