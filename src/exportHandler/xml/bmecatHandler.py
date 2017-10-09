fa60290b30cde0e4f66923b7bd63ab87eb3ab974
Henrik Pilz <henrik.pilz@gmail.com>
2017-10-09 20:48:32 +0200

    Exporter for BMEcats

--- src/exportHandler/xml/bmecatHandler.py
+++ src/exportHandler/xml/bmecatHandler.py
@@ -6,105 +6,27 @@ Created on 12.05.2017
 from datetime import datetime
 import csv
 import logging
-from xml.sax import handler
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring
-from ElementTree_pretty import prettify
-import datetime
 
-
-from data import *
-
-from xml.etree import ElementTree
-from xml.dom import minidom
+from lxml.etree import ElementTree, Element, SubElement, Comment, tostring
 
 
 
-class BMEcatHandler(handler.ContentHandler):
+class BMEcatHandler(ElementTree):
     
     ''' alle registrierten StartElementhandler '''
     _startElementHandler = {
-                "article" : "createProduct",
-                "article_details" : "createProductDetails", 
-                "order_details" : "createOrderDetails",
-                "price_details" : "createPriceDetails",
-                "price" : "createPrice",                
-                "mime" : "createMime",
-                "mime_info" : "startMimeInfo",
-                "datetime" : "startDateTime",
-                "article_features" :"createFeatureSet",
-                "feature" : "createFeature",
-                "special_treatment_class" : "createTreatmentClass",
-                "article_reference" : "createReference" 
-                }
-
-    ''' Mögliche Aliase für Varianten der BMEcats '''
-    _alias = {
-                "product" : "article",
-                "product_details" : "article_details",
-                "supplier_pid" : "supplier_aid",
-                "supplier_alt_pid" : "supplier_alt_aid",
-                "manufacturer_pid" : "manufacturer_aid",
-                "buyer_pid" : "buyer_aid",
-                "article_order_details" : "order_details",
-                "article_price_details" : "price_details",
-                "article_price" : "price",
-                "product_features" : "article_features",
-                "international_pid" : "ean",
-                "product_order_details" : "order_details",
-                "product_price_details" : "price_details",
-                "product_price" : "price",
-                "product_reference" : "article_reference",
-                "prod_id_to" : "art_id_to"
-            }
-            
-    ''' alle registrierten EndElementhandler '''
-    _endElementHandler = {
-                "article_features" :"saveFeatureSet",
-                "feature" : "saveFeature",
-                "article" : "saveProduct",
-                "mime_info" : "endMimeInfo",
-                "datetime" : "endDateTime",
-                "mime" : "saveMime",
-                "supplier_aid" : "addArticleId",
-                "supplier_alt_aid" : "addAlternativeArticleId",
-                "buyer_aid" : "addAlternativeArticleId",
-                "manufacturer_aid" : "addManufacturerArticleId",
-                "manufacturer_name" : "addManufacturerName",
-                "ean" : "addEAN", 
-                "description_long" : "addDescription",
-                "description_short" : "addTitle",
-                "price" : "savePrice",
-                "price_details" : "savePriceDetails",
-                "delivery_time" : "addDeliveryTime",
-                "price_amount" : "addPriceAmount",
-                "tax" : "addPriceTax",
-                "price_currency" : "addPriceCurrency",
-                "price_factor" : "addPriceFactor",
-                "territory" : "addTerritory",
-                "lower_bound" : "addPriceLowerBound",
-                "mime_source" : "addMimeSource",
-                "mime_type" : "addMimeType",
-                "mime_descr" : "addMimeDescription",
-                "mime_alt" : "addMimeAlt",
-                "mime_purpose" : "addMimePurpose",
-                "mime_order" : "addMimeOrder",
-                "order_unit" : "addOrderUnit",
-                "content_unit" : "addContentUnit",
-                "no_cu_per_ou " : "addPackagingQuantity",
-                "price_quantity " : "addPriceQuantity",
-                "quantity_min " : "addQuantityMin",
-                "quantity_interval" : "addQuantityInterval",
-                "date" : "addDate",
-                "fname" : "addFeatureName",
-                "fvalue" : "addFeatureValue",
-                "fvalue_details" : "addFeatureValueDetails",
-                "funit" : "addFeatureUnit",
-                "fdesc" : "addFeatureDescription",
-                "special_treatment_class" : "saveTreatmentClass",
-                "catalog_group_system" : "resetAll",
-                "article_reference" : "saveReference",
-                "art_id_to" : "addReferenceArticleId",
-                "reference_descr" : "addReferenceDescription"
+                "article" : "Product",
+                "article_details" : "ProductDetails", 
+                "order_details" : "OrderDetails",
+                "price_details" : "PriceDetails",
+                "price" : "Price",                
+                "mime" : "Mime",
+                "mime_info" : "mimeInfo",
+                "datetime" : "DateTime",
+                "article_features" :"FeatureSet",
+                "feature" : "Feature",
+                "special_treatment_class" : "TreatmentClass",
+                "article_reference" : "Reference" 
                 }
 
     def createHeader(self):
@@ -167,9 +89,9 @@ class BMEcatHandler(handler.ContentHandler):
             for i in xrange(3)
             ]
 
-top.extend(children)
+        top.extend(children)
 
-print prettify(root)
+        print prettify(root)
         
         print self.prettify(top)
 
