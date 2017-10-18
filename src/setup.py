7405813c660272cd78907a4d9d09a30562d23402
Henrik Pilz <HenrikPilz@users.noreply.github.com>
2017-10-18 21:07:46 +0200

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='BMEcatConverter',
+      version='1.0',
+      py_modules=['main', 'converter', 'argumentParser', 'data', 'packageInstaller', 'exportHandler', 'importHandler', 'mapping', 'test', 'resolver', 'transformer' ]
+      )
