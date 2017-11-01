d5270668e24fbbbbd1460368849e17b547ac5f95
Henrik Pilz <henrik.pilz@gmail.com>
2017-11-01 13:32:40 +0100

    Fix missing detection of old Mapping-Master-Fields

--- src/importer/excel/excelImporter.py
+++ src/importer/excel/excelImporter.py
@@ -144,7 +144,8 @@ class ExcelImporter(object):
                     # dann muessen wir splitten, da der Zahlenanteil
                     # Sowohl die Zusammengehörigkeit der Daten anzeigt,
                     # als auch die Reihenfolge bestimmt
-                    fieldName = regex.match("[a-zA-Z]*", currentFieldname).group(0)
+                    fieldName = regex.match("[a-zA-Z_]*", currentFieldname).group(0)
+                    fieldName = fieldName.stri("_")
                     fieldCount = currentFieldname.replace(fieldName, "").strip("_")
                     
                     logging.debug("'{0}' : '{1}'".format(fieldName, fieldCount))
