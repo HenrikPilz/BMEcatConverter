20aeecf25e4fddb085db9b2b41985c51bd7cf432
Henrik Pilz <henrik.pilz@gmail.com>
2017-11-09 20:45:32 +0100

    Codestyle Adaption

--- src/argumentParser.py
+++ src/argumentParser.py
@@ -7,74 +7,80 @@ Created on 01.09.2017
 import getopt
 import logging
 
+
 class HelpCalledException(Exception):
     '''
     Exception if Help is called
     '''
+
 
 class MissingArgumentException(Exception):
     '''
     Exception if an argument is Missing
     '''
 
+
 class ArgumentParser():
     '''
     classdocs
     '''
     def __init__(self):
-        self._inputfile = None
-        self._outputfile = None
-        self._merchant = 'fiege'
-        self._manufacturer = None
-        self._dateformat = None
-        self._separatorMode = None
-        
+        self.inputfile = None
+        self.outputfile = None
+        self.merchant = 'fiege'
+        self.manufacturer = None
+        self.dateformat = None
+        self.separatorMode = None
+
     def parse(self, argv):
-        opts, args = getopt.getopt(argv,"hi:o:", ["merchant=", "manufacturer=", "dateformat=", "separators="])
+        opts, _ = getopt.getopt(
+                                argv,
+                                "hi:o:",
+                                ["merchant=",
+                                 "manufacturer=",
+                                 "dateformat=",
+                                 "separators="]
+                                   )
         print("Options: ", opts)
-    
+
         for opt, arg in opts:
             logging.debug("Option: " + opt)
             logging.debug("Argument: " + arg)
-            self._checkAndDetermineArgument(opt, arg)            
-                
+            self._checkAndDetermineArgument(opt, arg)
+
         self._validateArguments()
         self.logData()
-        
-        return self._inputfile, self._outputfile, self._dateformat, self._separatorMode, self._manufacturer, self._merchant
 
     def _checkAndDetermineArgument(self, opt, arg):
         if opt == '-h':
             raise HelpCalledException()
         if opt == "-i":
-            self._inputfile = arg
+            self.inputfile = arg
         if opt == "-o":
-            self._outputfile = arg
+            self.outputfile = arg
         if opt == "--manufacturer":
-            self._manufacturer = arg
+            self.manufacturer = arg
         if opt == "--merchant":
-            self._merchant = arg
+            self.merchant = arg
         if opt == "--separators":
-            self._separatorMode = arg
+            self.separatorMode = arg
         if opt == "--dateformat":
-            self._dateformat = arg
+            self.dateformat = arg
 
     def _validateArguments(self):
-        if self._inputfile is None:
+        if self.inputfile is None:
             raise MissingArgumentException("Inputfile is missing.")
-        if self._outputfile is None:
+        if self.outputfile is None:
             raise MissingArgumentException("Outputfile is missing.")
-        if self._dateformat is None:
+        if self.dateformat is None:
             raise MissingArgumentException("Dateformat  is missing.")
-        if self._separatorMode is None:
+        if self.separatorMode is None:
             raise MissingArgumentException("SeparateorMode  is missing.")
 
     def logData(self):
-        logging.info("Input file is {0}".format(self._inputfile))
-        logging.info("Output file is {0}".format(self._outputfile))
-        if self._merchant is not None:
-            logging.info("Merchant: {0}".format(self._merchant))
-        if self._manufacturer is not None:
-            logging.info("Manufacturer: {0}".format(self._manufacturer))
-    
-
+        logging.info("Input file is {0}".format(self.inputfile))
+        logging.info("Output file is {0}".format(self.outputfile))
+        if self.merchant is not None:
+            logging.info("Merchant: {0}".format(self.merchant))
+        if self.manufacturer is not None:
+            logging.info("Manufacturer: {0}".format(self.manufacturer))
--- src/main.py
+++ src/main.py
@@ -1,46 +1,55 @@
 from __future__ import print_function
 
+import getopt
+import logging
 import os
 import sys
-import logging
-import getopt
-
 import time
 
 from argumentParser import ArgumentParser
-from argumentParser import MissingArgumentException
 from argumentParser import HelpCalledException
-import packageInstaller
-from converter import Converter
+from argumentParser import MissingArgumentException
 from converter import ConversionModeException
+from converter import Converter
+import packageInstaller
+
 
 loggingLevel = logging.INFO
+
 
 def printHelp():
     """
     Hilfe ausgeben
     """
-    print("python main.py -i <inputfile> -o <outputfile> --dateformat \"%Y-%m-%d\" --separators english")
+    print("python main.py -i <inputfile> -o <outputfile>" +
+          " --dateformat \"%Y-%m-%d\" --separators english")
     print("\n")
     print("There are two modes in which the converter can be used:")
-    print("\t1.\tInput XML and Output xlsx: This means converting from any BMEcat format to ")
+    print("\t1.\tInput XML and Output xlsx: " +
+          "This means converting from any BMEcat format to ")
     print("\t--dateformat <dateformat>")
-    print("\t\te.g. '%Y-%m-%d' or '%d.%m.%Y' or anything else with Y as Year, d as day and m as month ")
+    print("\t\te.g. '%Y-%m-%d' or '%d.%m.%Y' or anything else with Y as Year" +
+          ", d as day and m as month ")
     print("\t--separators <english|german|detect>")
-    print("\t\te.g. -separators german leads to numbers beeing expected like 10.000,00")
-    print("\t\t     -separators english leads to numbers beeing expected like 10,000.00")
-    print("\t\t     -separators detect tries to detect what could be there (unsafe).")
+    print("\t\te.g. -separators german leads to numbers" +
+          " beeing expected like 10.000,00")
+    print("\t\t     -separators english leads to numbers" +
+          " beeing expected like 10,000.00")
+    print("\t\t     -separators detect tries to detect" +
+          " what could be there (unsafe).")
     print("Optionally:")
     print("\t--merchant <Merchantname>")
     print("\t--manufacturer <Manufacturername>")
-    print("\ti.e. python main.py -i makita_bmecat.xml -o makita_excelfilname.xlsx -merchant \"Contorion\" -manufacturer \"Makita\"")
+    print("\ti.e. python main.py -i makita_bmecat.xml" +
+          " -o makita_excelfilname.xlsx -merchant \"Contorion\"" +
+          " -manufacturer \"Makita\"")
 
 
-'''
-    setUp Logging for File and Console
-'''
-
-def createFileLoggingHandler(logfilename, logLevel=logging.DEBUG, logFormat='%(levelname)7s: %(message)s'):
+def createFileLoggingHandler(logfilename, logLevel=logging.DEBUG,
+                             logFormat='%(levelname)7s: %(message)s'):
+    '''
+    setUp Logging for File
+    '''
     if os.path.exists(logfilename):
         os.remove(logfilename)
     ''' log File'''
@@ -52,6 +61,9 @@ def createFileLoggingHandler(logfilename, logLevel=logging.DEBUG, logFormat='%(l
 
 
 def configureStdoutLogging():
+    '''
+    setUp Logging for Console
+    '''
     frmStdOut = logging.Formatter('%(levelname)7s - %(message)s')
     ''' Console out '''
     stdOutHandler = logging.StreamHandler(sys.stdout)
@@ -59,46 +71,56 @@ def configureStdoutLogging():
     stdOutHandler.setLevel(loggingLevel)
     return stdOutHandler
 
+
 def setUpLogging():
+    '''
+    setUp Logging for File and Console
+    '''
     logger = logging.getLogger()
-    
-    ''' Debug Log File '''
-    debugLogFileHandler = createFileLoggingHandler(logfilename="convert_debug.log", logFormat='%(levelname)7s - [%(filename)20s:%(lineno)s - %(funcName)20s()]: %(message)s')
+
+    # Debug Log File
+    fmt = '%(levelname)7s - [%(filename)20s:%(lineno)s - %(funcName)20s()]: %(message)s'
+    debugLogFileHandler = createFileLoggingHandler(
+                                        logfilename="convert_debug.log",
+                                        logFormat=fmt)
     logger.addHandler(debugLogFileHandler)
 
     ''' Common Log File for Validation etc.'''
-    logFileHandler = createFileLoggingHandler(logfilename="convert.log", logLevel=logging.WARNING) 
+    logFileHandler = createFileLoggingHandler(logfilename="convert.log",
+                                              logLevel=logging.WARNING)
     logger.addHandler(logFileHandler)
-    
+
     stdOutHandler = configureStdoutLogging()
-    logger.addHandler(stdOutHandler) 
+    logger.addHandler(stdOutHandler)
     logger.setLevel(loggingLevel)
-    
+
+
 if __name__ == '__main__':
     # Check for openpyxl
     packageInstaller.installIfNeeded("openpyxl")
     packageInstaller.installIfNeeded("regex")
     packageInstaller.installIfNeeded("lxml")
 
-    # Loging einstgellen: zwei Outputdateien plus Konsole 
+    # Loging einstgellen: zwei Outputdateien plus Konsole
     setUpLogging()
 
     logging.debug('Number of arguments:', len(sys.argv), 'arguments.')
     logging.debug('Argument List:', str(sys.argv))
     t1 = time.clock()
-    
+
     try:
         argv = sys.argv[1:]
-        inputFilename, outputFilename, dateFormat, separatorMode, manufacturerName, merchantName = ArgumentParser().parse(argv)
+        argumentParser = ArgumentParser()
+        argumentParser.parse(argv)
         config = {
-            'inputfile' : inputFilename, 
-            'outputfile' : outputFilename,
-            'dateFormat' : dateFormat,
-            'separatorMode' : separatorMode,
-            'manufacturerName': manufacturerName,
-            'merchant' : merchantName
+            'inputfile' : argumentParser.inputFilename,
+            'outputfile' : argumentParser.outputFilename,
+            'dateFormat' : argumentParser.dateFormat,
+            'separatorMode' : argumentParser.separatorMode,
+            'manufacturerName': argumentParser.manufacturerName,
+            'merchant' : argumentParser.merchantName
         }
-        converter = Converter(config)        
+        converter = Converter(config)
         converter.convert()
     except FileNotFoundError as fnfe:
         print("Dateiname konnte nicht gefunden werden: ", str(fnfe))
@@ -117,7 +139,7 @@ if __name__ == '__main__':
         print("Error: ")
         printHelp()
         sys.exit(4)
-    
+
     t2 = time.clock()
     duration = t2-t1
     if duration < 60:
