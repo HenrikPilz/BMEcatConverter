6529268a191102aba3dcb1cc09449d27848f1299
Henrik Pilz <henrik.pilz@gmail.com>
2017-11-17 17:46:37 +0100

    fix in validating ob ject

--- src/datamodel/product.py
+++ src/datamodel/product.py
@@ -9,11 +9,11 @@ from lxml.etree import SubElement
 
 from . import ValidatingXMLObject, ComparableEqual
 from .productDetails import ProductDetails
-from .orderDetails import OrderDetails 
+from .orderDetails import OrderDetails
 
-class Product(ValidatingXMLObject, ComparableEqual):
+class Product( ValidatingXMLObject, ComparableEqual ):
 
-    def __init__(self):        
+    def __init__( self ):
         self.productId = None
         self.details = None
         self.orderDetails = None
@@ -26,119 +26,117 @@ class Product(ValidatingXMLObject, ComparableEqual):
         self.variants = []
         self.numberOfVariants = 1
 
-    def __eq__(self, other):
-        if not super().__eq__(other):
+    def __eq__( self, other ):
+        if not super().__eq__( other ):
             return False
         else:
-            priceDetailsEqual = super().checkListForEquality(self.priceDetails, other.priceDetails)
-            mimeInfoEqual = super().checkListForEquality(self.mimeInfo, other.mimeInfo)        
-            userDefinedExtensionsEqual = super().checkListForEquality(self.userDefinedExtensions, other.userDefinedExtensions)
-            featureSetsEqual = super().checkListForEquality(self.featureSets, other.featureSets)
-            referencesEqual = super().checkListForEquality(self.references, other.references)
-            variantsEqualEqual = super().checkListForEquality(self.variants, other.variants)
-            productIdEqual = str(self.productId) == str(other.productId)
-                    
-            return priceDetailsEqual and mimeInfoEqual and userDefinedExtensionsEqual and featureSetsEqual and referencesEqual and variantsEqualEqual and productIdEqual and self.details == other.details and self.orderDetails == other.orderDetails and self.hasVariants == other.hasVariants
-    
-    def __ne__(self, other):
-        return not self.__eq__(other)
-   
-    def validate(self, raiseException=False):
-        super().valueNotNone(self.productId, "Der Artikel hat keine Artikelnummer.", raiseException)
-        messagePrefix = "Der Artikel '{0}' hat ".format(self.productId)
-        super().valueNotNone(self.details, messagePrefix + "keine Artikeldetails.".format(self.productId), raiseException)
-        try:
-            self.details.validate(raiseException)
-        except Exception as e:
-            raise Exception(messagePrefix + "fehlerhafte Artikeldetails. " + str(e))
-        super().valueNotNone(self.orderDetails, messagePrefix + "keine Bestellinformation.".format(self.productId), raiseException)
-        try:
-            self.orderDetails.validate(raiseException)
-        except Exception as e:
-            raise Exception(messagePrefix + "fehlerhafte Bestellinformationen. " + str(e))
-        super().valueNotNoneOrEmpty(self.priceDetails, messagePrefix + "keine Preisinformationen.".format(self.productId), raiseException)
-        super().validateList(self.priceDetails, messagePrefix + "fehlerhafte Preisinformationen.", raiseException)
-            
-        if super().valueNotNoneOrEmpty(self.mimeInfo, messagePrefix + "keine Bilder.".format(self.productId), False):
-            super().validateList(self.mimeInfo, messagePrefix + "fehlerhafte Bildinformationen.", raiseException)
+            priceDetailsEqual = super().checkListForEquality( self.priceDetails, other.priceDetails )
+            mimeInfoEqual = super().checkListForEquality( self.mimeInfo, other.mimeInfo )
+            userDefinedExtensionsEqual = super().checkListForEquality( self.userDefinedExtensions, other.userDefinedExtensions )
+            featureSetsEqual = super().checkListForEquality( self.featureSets, other.featureSets )
+            referencesEqual = super().checkListForEquality( self.references, other.references )
+            variantsEqualEqual = super().checkListForEquality( self.variants, other.variants )
+            productIdEqual = str( self.productId ) == str( other.productId )
 
-        if super().valueNotNoneOrEmpty(self.featureSets, messagePrefix + "keine Attribute.".format(self.productId), False):
-            super().validateList(self.featureSets,messagePrefix + "fehlerhafte Attributinformationen.", raiseException)
-    
-    def addDetails(self):
+            return priceDetailsEqual and mimeInfoEqual and userDefinedExtensionsEqual and featureSetsEqual and referencesEqual and variantsEqualEqual and productIdEqual and self.details == other.details and self.orderDetails == other.orderDetails and self.hasVariants == other.hasVariants
+
+    def __ne__( self, other ):
+        return not self.__eq__( other )
+
+    def validate( self, raiseException = False ):
+        super().valueNotNone( self.productId, "Der Artikel hat keine Artikelnummer.", raiseException )
+        messagePrefix = "Der Artikel '{0}' hat ".format( self.productId )
+        super().valueNotNone( self.details, messagePrefix + "keine Artikeldetails.".format( self.productId ), raiseException )
+        try:
+            self.details.validate( raiseException )
+        except Exception as e:
+            raise Exception( messagePrefix + "fehlerhafte Artikeldetails. " + str( e ) )
+        super().valueNotNone( self.orderDetails, messagePrefix + "keine Bestellinformation.".format( self.productId ), raiseException )
+        try:
+            self.orderDetails.validate( raiseException )
+        except Exception as e:
+            raise Exception( messagePrefix + "fehlerhafte Bestellinformationen. " + str( e ) )
+        super().valueNotNoneOrEmpty( self.priceDetails, messagePrefix + "keine Preisinformationen.".format( self.productId ), raiseException )
+        super().validateList( self.priceDetails, messagePrefix + "fehlerhafte Preisinformationen.", raiseException )
+
+        if super().valueNotNoneOrEmpty( self.mimeInfo, messagePrefix + "keine Bilder.".format( self.productId ), False ):
+            super().validateList( self.mimeInfo, messagePrefix + "fehlerhafte Bildinformationen.", raiseException )
+
+        if super().valueNotNoneOrEmpty( self.featureSets, messagePrefix + "keine Attribute.".format( self.productId ), False ):
+            super().validateList( self.featureSets, messagePrefix + "fehlerhafte Attributinformationen.", raiseException )
+
+    def addDetails( self ):
         self.details = ProductDetails()
-    
-    def addOrderDetails(self):
+
+    def addOrderDetails( self ):
         self.orderDetails = OrderDetails()
 
-    def addPriceDetails(self, priceDetails, raiseException=True):
-        if priceDetails is not None:
-            self.addToListIfValid(priceDetails, self.priceDetails, "Die Preisdetails enthalten keine validen Einträge. Sie werden nicht hinzugefuegt.", raiseException)
-        
-    def addTitle(self, title):
+    def addPriceDetails( self, priceDetails, raiseException = True ):
+        self.addToListIfValid( priceDetails, self.priceDetails, "Die Preisdetails enthalten keine validen Einträge. Sie werden nicht hinzugefuegt.", raiseException )
+
+    def addTitle( self, title ):
         self.details.title = title
 
-    def addDescription(self, description):
+    def addDescription( self, description ):
         self.details.description = description
 
-    def addManufacturerArticleId(self, manufacturerArticleId):
+    def addManufacturerArticleId( self, manufacturerArticleId ):
         self.details.manufacturerArticleId = manufacturerArticleId
 
-    def addManufacturerName(self, manufacturerName):
+    def addManufacturerName( self, manufacturerName ):
         self.details.manufacturerName = manufacturerName
 
-    def addEAN(self, ean):
+    def addEAN( self, ean ):
         self.details.ean = ean
-    
-    def addDeliveryTime(self, deliveryTime):
+
+    def addDeliveryTime( self, deliveryTime ):
         self.details.deliveryTime = deliveryTime
-        
-    def addMime(self, mime, raiseException=True):
-        if mime is not None:
-            self.addToListIfValid(mime, self.mimeInfo, "Das Bild enthaelt keine validen Einträge. Es wird nicht hinzugefuegt.", raiseException)
 
-    def addReference(self, reference):
-        self.references.append(reference)
+    def addMime( self, mime, raiseException = True ):
+        self.addToListIfValid( mime, self.mimeInfo, "Das Bild enthaelt keine validen Einträge. Es wird nicht hinzugefuegt.", raiseException )
 
-    def addUserDefinedExtension(self, udf):
+    def addReference( self, reference, raiseException = True ):
+        self.addToListIfValid( reference, self.references, "Die Referenz ist nicht valide. Sie wird nicht hinzugefügt.", raiseException )
+
+    def addUserDefinedExtension( self, udf ):
         '''self.userDefinedExtensions.append(udf)'''
         pass
-        
-    def addSpecialTreatmentClass(self, treatmentclass):
-        self.details.addSpecialTreatmentClass(treatmentclass)
-        
-    def addKeyword(self, keyword):
-        self.details.addKeyword(keyword)
 
-    def addFeatureSet(self, featureSet):
+    def addSpecialTreatmentClass( self, treatmentclass ):
+        self.details.addSpecialTreatmentClass( treatmentclass )
+
+    def addKeyword( self, keyword ):
+        self.details.addKeyword( keyword )
+
+    def addFeatureSet( self, featureSet ):
         message = None
         if self.productId is not None:
-            message = "Artikel '{0}' Attributset ist leer und wird nicht gespeichert.".format(self.productId)
+            message = "Artikel '{0}' Attributset ist leer und wird nicht gespeichert.".format( self.productId )
         else:
             message = "Das Attributset ist leer und wird nicht gespeichert."
-        if super().addToListIfValid(featureSet, self.featureSets, message):
+        if super().addToListIfValid( featureSet, self.featureSets, message ):
             for feature in featureSet.features:
                 if feature.hasVariants():
-                    logging.info("Variante gefunden.")
-                    self.__addVariant(feature)
+                    logging.info( "Variante gefunden." )
+                    self.__addVariant( feature )
         else:
-            logging.debug(message)
-    
-    def __addVariant(self, feature):
-        self.variants.append((feature.variants.order, feature.name, feature.variants))
+            logging.debug( message )
+
+    def __addVariant( self, feature ):
+        self.variants.append( ( feature.variants.order, feature.name, feature.variants ) )
         # Anzahl bestehender Varianter multipliziert mit der Anzahl definierter Varianter in diesem Feature
-        self.numberOfVariants *= len(feature.variants)
+        self.numberOfVariants *= len( feature.variants )
         self.hasVariants = self.numberOfVariants > 1
-    
-    def toXml(self, articleType='new', raiseExceptionOnValidate=True):
-        articleElement = super().validateAndCreateBaseElement("ARTICLE", { "mode" : articleType }, raiseExceptionOnValidate)
-        super().addMandatorySubElement(articleElement, "SUPPLIER_AID", self.productId)
-        articleElement.append(self.details.toXml(raiseExceptionOnValidate))
-        articleElement.append(self.orderDetails.toXml(raiseExceptionOnValidate))
-        super().addListOfSubElements(articleElement, self.priceDetails, raiseExceptionOnValidate)
-        if super().valueNotNoneOrEmpty(self.mimeInfo):
-            mimeInfoElement = SubElement(articleElement, "MIME_INFO")        
-            super().addListOfSubElements(mimeInfoElement, sorted(self.mimeInfo, key=lambda mime: int(mime.order)), raiseExceptionOnValidate)
-        super().addListOfSubElements(articleElement, self.featureSets, raiseExceptionOnValidate)
-        super().addListOfSubElements(articleElement, self.references, raiseExceptionOnValidate)        
+
+    def toXml( self, articleType = 'new', raiseExceptionOnValidate = True ):
+        articleElement = super().validateAndCreateBaseElement( "ARTICLE", { "mode" : articleType }, raiseExceptionOnValidate )
+        super().addMandatorySubElement( articleElement, "SUPPLIER_AID", self.productId )
+        articleElement.append( self.details.toXml( raiseExceptionOnValidate ) )
+        articleElement.append( self.orderDetails.toXml( raiseExceptionOnValidate ) )
+        super().addListOfSubElements( articleElement, self.priceDetails, raiseExceptionOnValidate )
+        if super().valueNotNoneOrEmpty( self.mimeInfo ):
+            mimeInfoElement = SubElement( articleElement, "MIME_INFO" )
+            super().addListOfSubElements( mimeInfoElement, sorted( self.mimeInfo, key = lambda mime: int( mime.order ) ), raiseExceptionOnValidate )
+        super().addListOfSubElements( articleElement, self.featureSets, raiseExceptionOnValidate )
+        super().addListOfSubElements( articleElement, self.references, raiseExceptionOnValidate )
         return articleElement
--- src/datamodel/validatingObject.py
+++ src/datamodel/validatingObject.py
@@ -10,25 +10,25 @@ import logging
 from lxml.etree import SubElement, Element
 
 
-class NoValueGivenException(Exception):
+class NoValueGivenException( Exception ):
     '''
     Exception thrown when no Value is given.
     '''
 
 
-class ComparableEqual(object):
+class ComparableEqual( object ):
     '''
     Interface Class for a Class which are comparable
     '''
-    
-    def __eq__(self, other):
-        return isinstance(other, self.__class__)
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+    def __eq__( self, other ):
+        return isinstance( other, self.__class__ )
+
+    def __ne__( self, other ):
+        return not self.__eq__( other )
 
     @classmethod
-    def checkListForEquality(self, lhs, rhs):
+    def checkListForEquality( self, lhs, rhs ):
         for leftItem in lhs:
             if leftItem not in rhs:
                 return False
@@ -37,137 +37,139 @@ class ComparableEqual(object):
             if rightItem not in lhs:
                 return False
         return True
-    
 
-class ValidatingObject(object):
+
+class ValidatingObject( object ):
     '''
     Interface Class for a Class which validates its content
     '''
-    
-    def add(self, attributeName, attributeValue):
+
+    def add( self, attributeName, attributeValue ):
         try:
-            if not isinstance(getattr(self, attributeName), (list, array)):
-                setattr(self, attributeName, attributeValue)
+            if not isinstance( getattr( self, attributeName ), ( list, array ) ):
+                setattr( self, attributeName, attributeValue )
             else:
-                getattr(self, attributeName).append(attributeValue)
+                getattr( self, attributeName ).append( attributeValue )
         except AttributeError as ae:
-            logging.error("Klassenattribut nicht gefunden: ", str(ae))
-    
-    def validateList(self, listToValidate, additionalMessage, raiseException):
+            logging.error( "Klassenattribut nicht gefunden: ", str( ae ) )
+
+    def validateList( self, listToValidate, additionalMessage, raiseException ):
         try:
             for listItem in listToValidate:
-                listItem.validate(raiseException)
+                listItem.validate( raiseException )
         except Exception as e:
-            raise Exception(additionalMessage + " :: " + str(e))
-    
+            raise Exception( additionalMessage + " :: " + str( e ) )
+
     @abstractmethod
-    def validate(self, raiseException=False):
-        raise NotImplementedError("Please implement 'validate' in your class '{0}".format(__file__))
-    
-    def valueNotNone(self, attribute, message=None, raiseException=False):
+    def validate( self, raiseException = False ):
+        raise NotImplementedError( "Please implement 'validate' in your class '{0}".format( __file__ ) )
+
+    def valueNotNone( self, attribute, message = None, raiseException = False ):
         if attribute is None:
-            self.logError(message, raiseException)
+            self.logError( message, raiseException )
             return False
         return True
 
-    def valueNotNoneOrEmpty(self, attribute, message=None, raiseException=False):
-        if not self.valueNotNone(attribute, message, raiseException):
+    def valueNotNoneOrEmpty( self, attribute, message = None, raiseException = False ):
+        if not self.valueNotNone( attribute, message, raiseException ):
             return False
         isNotEmpty = True
-        if isinstance(attribute, str):
-            isNotEmpty = len(attribute.strip()) > 0                
-        elif isinstance(attribute, (list, array)):
-            isNotEmpty = len(attribute) > 0
+        if isinstance( attribute, str ):
+            isNotEmpty = len( attribute.strip() ) > 0
+        elif isinstance( attribute, ( list, array ) ):
+            isNotEmpty = len( attribute ) > 0
         else:
-            isNotEmpty = len(str(attribute).strip()) > 0
+            isNotEmpty = len( str( attribute ).strip() ) > 0
         if isNotEmpty:
             return True
-        self.logError(message, raiseException)
+        self.logError( message, raiseException )
         return False
 
-    
-    def logError(self, errMsg=None, raiseException=False):
+
+    def logError( self, errMsg = None, raiseException = False ):
         '''
          Taking an ErrorMessage, logging it and if wanted throwing an Exception
         '''
         if errMsg is not None:
-            logging.error(errMsg)
+            logging.error( errMsg )
         if raiseException:
-            raise Exception(errMsg)
+            raise Exception( errMsg )
 
-    def addToListIfValid(self, item, listToAddTo, errorMessage, raiseException=True):
+    def addToListIfValid( self, item, listToAddTo, errorMessage, raiseException = True ):
+        if item is None:
+            return False
         try:
-            self.__determineOrderIfNeeded(item, listToAddTo)
-            item.validate(raiseException)
+            self.__determineOrderIfNeeded( item, listToAddTo )
+            item.validate( raiseException )
             if item not in listToAddTo:
-                listToAddTo.append(item)
+                listToAddTo.append( item )
                 return True
         except Exception as ve:
-            logging.warning(errorMessage + "{0}".format(str(ve)))
+            logging.warning( errorMessage + "{0}".format( str( ve ) ) )
         return False
 
 
-    def __determineOrderIfNeeded(self, item, orderedList):
+    def __determineOrderIfNeeded( self, item, orderedList ):
         try:
-            if getattr(item, "order") is None or int(item.order) <= 0:
-                self.__determineOrder(item, orderedList)
+            if getattr( item, "order" ) is None or int( item.order ) <= 0:
+                self.__determineOrder( item, orderedList )
         except AttributeError:
             pass
 
-    def __determineOrder(self, item, orderedList):
-        maxItem = max(orderedList, key=lambda item: int(item.order), default=None)
-        if maxItem is None:             
+    def __determineOrder( self, item, orderedList ):
+        maxItem = max( orderedList, key = lambda item: int( item.order ), default = None )
+        if maxItem is None:
             item.order = 1
         else:
-            item.order = int(maxItem.order) + 1
+            item.order = int( maxItem.order ) + 1
 
 
-class XMLObject(object):
+class XMLObject( object ):
     '''
     Interface Class for a Class which writes its content to XML
     '''
-    
+
     @abstractmethod
-    def toXml(self):
-        raise NotImplementedError("Please implement 'toXml' in your class '{0}".format(__file__))
-    
-    def addMandatorySubElement(self, parent, tag, value):
+    def toXml( self ):
+        raise NotImplementedError( "Please implement 'toXml' in your class '{0}".format( __file__ ) )
+
+    def addMandatorySubElement( self, parent, tag, value ):
         '''
         Erstellt eiin Child-Element und wirft eine Exception, falls value nicht None
         '''
         if value is None:
-            raise NoValueGivenException("Kein Wert übergeben.")
-        subElement = SubElement(parent, tag)
-        subElement.text = str(value)
+            raise NoValueGivenException( "Kein Wert übergeben." )
+        subElement = SubElement( parent, tag )
+        subElement.text = str( value )
         return subElement
-        
-    
-    def addOptionalSubElement(self, parent, tag, value):
+
+
+    def addOptionalSubElement( self, parent, tag, value ):
         '''
         Erstellt eiin Child-Element, falls value nicht None
         '''
-        if value is not None and len(str(value)) > 0:
-            return self.addMandatorySubElement(parent, tag, value)
+        if value is not None and len( str( value ) ) > 0:
+            return self.addMandatorySubElement( parent, tag, value )
         return None
 
-    def addListOfSubElements(self, parent, listOfSubElements, raiseExceptionOnValidate=True):
+    def addListOfSubElements( self, parent, listOfSubElements, raiseExceptionOnValidate = True ):
         for element in listOfSubElements:
-            parent.append(element.toXml(raiseExceptionOnValidate=raiseExceptionOnValidate))
+            parent.append( element.toXml( raiseExceptionOnValidate = raiseExceptionOnValidate ) )
 
-    def addDateTimeSubElement(self, parent, dateType, date):
-        dateTimeSubElement = SubElement(parent, "DATETIME", { "type"  : dateType})
-        dateSubElement = SubElement(dateTimeSubElement, "DATE")
-        dateSubElement.text = date.strftime("%Y-%m-%d")
-        timeSubElement = SubElement(dateTimeSubElement, "TIME")
-        timeSubElement.text = date.strftime("%H:%M:%S")
+    def addDateTimeSubElement( self, parent, dateType, date ):
+        dateTimeSubElement = SubElement( parent, "DATETIME", { "type"  : dateType} )
+        dateSubElement = SubElement( dateTimeSubElement, "DATE" )
+        dateSubElement.text = date.strftime( "%Y-%m-%d" )
+        timeSubElement = SubElement( dateTimeSubElement, "TIME" )
+        timeSubElement.text = date.strftime( "%H:%M:%S" )
 
 
-class ValidatingXMLObject(ValidatingObject, XMLObject):
+class ValidatingXMLObject( ValidatingObject, XMLObject ):
     '''
     Interface Class for a Class which validates its content and writes its Content to XML
     '''
-    
-    def validateAndCreateBaseElement(self, tagname, attributes=None, raiseExceptionOnValidate=True):
-        self.validate(raiseExceptionOnValidate)
-        return Element(tagname, attributes)
+
+    def validateAndCreateBaseElement( self, tagname, attributes = None, raiseExceptionOnValidate = True ):
+        self.validate( raiseExceptionOnValidate )
+        return Element( tagname, attributes )
 
