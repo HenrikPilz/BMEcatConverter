f74902abe4bebd9b8fe24c26de87c3fd1a19b8ba
Henrik Pilz <HenrikPilz@users.noreply.github.com>
2017-11-20 21:17:20 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(name='BMEcatConverter',
-      description='This is a converter, which appies for two tasks:\n\t- converting BMEcats into Excel-Sheets' +
-                  '\n\t- converting Excel-Sheets of certain form(Mapping-Master) into BMEcat Version 1.2',
+      description='This is a converter, which applies for two tasks:\n\t- converting BMEcats into Excel-Sheets' +
+                  '\n\t- converting Excel-Sheets of certain form (Mapping-Master) into BMEcat Version 1.2',
       version='1.2',
       author='Henrik Pilz',
       author_email='henrik.pilz@contorion.de',
