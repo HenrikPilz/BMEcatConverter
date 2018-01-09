bb892e20c9686840fbd09095ea87635b56947eea
Henrik Pilz <henrik.pilz@gmail.com>
2018-01-09 17:19:30 +0100

    Versionsnummer erhöht

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='BMEcatConverter',
       description='This is a converter, which applies for two tasks:\n\t- converting BMEcats into Excel-Sheets' +
                   '\n\t- converting Excel-Sheets of certain form (Mapping-Master) into BMEcat Version 1.2',
-      version='1.2',
+      version='1.3',
       author='Henrik Pilz',
       author_email='henrik.pilz@contorion.de',
       packages=[ 'datamodel', 'exporter', 'importer', 'mapping', 'test', 'resolver', 'transformer' ],
